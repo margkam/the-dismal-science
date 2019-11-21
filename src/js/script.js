0dbc7f8a14faaedfe4449d1bcd24792833227292
Margaret Watkins <margaret.watkins@aggiemail.usu.edu>
2019-11-21 12:48:10 -0700

    Indicators chart: add business investment and toggle between indicators

--- src/js/script.js
+++ src/js/script.js
@@ -9,9 +9,11 @@ let indicatorsChart = new IndicatorsChart();
 Promise.all([
         d3.csv("data/US_recessions.csv"),
         d3.csv("data/US_yield_curve.csv"),
+        d3.csv("data/US_investment.csv"),
     ]).then(datasets => {
         let recessions = datasets[0];
         let yieldCurve = datasets[1];
+        let investment = datasets[2];
 
         recessions.map(entry => {
             entry.DATE = Date.parse(entry.DATE);
@@ -19,11 +21,18 @@ Promise.all([
         
         yieldCurve.map(entry => {
             entry.DATE = Date.parse(entry.DATE);
-            entry.T10Y3MM = Number.parseFloat(entry.T10Y3MM);
+            entry.T10Y3MM = +entry.T10Y3MM;
+
         })
 
+        investment.map(entry => {
+            entry.DATE = Date.parse(entry.DATE);
+            entry.GPDI = +entry.GPDI;
+        })
+
+        //console.log(investment);
         //console.log(recessions);
         //console.log(yieldCurve);
-        indicatorsChart.display(recessions, yieldCurve);
+        indicatorsChart.display(recessions, yieldCurve, investment);
     })
 ;
