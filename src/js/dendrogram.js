f374ee1fa355a07e1df604ad6dc0570984bde694
Margaret Watkins <margaret.watkins@aggiemail.usu.edu>
2019-12-02 15:57:58 -0700

    Fta chart: Display and parse data by year

--- src/js/dendrogram.js
+++ src/js/dendrogram.js
@@ -4,25 +4,36 @@
 class Dendrogram {
     constructor() {
         this.config = {
-            width: 800,
+            listWidth: 150,
+            width: 1000,
         }
 
-        this.svg = this.svg = d3.select('#trade-chart')
+        this.listSvg = d3.select('#trade-chart')
             .append('svg')
+            .attr('width', this.config.listWidth)
+            .attr('height', this.config.width)
+        ;
+        
+        this.dendroSvg = d3.select('#trade-chart')
+            .append('svg')
+            .attr('width', this.config.width)
+            .attr('height', this.config.width)
             .attr("viewBox", [-this.config.width / 2, -this.config.width / 2, this.config.width, this.config.width])
         ;
+
+        
+        this.yearList = this.listSvg.append('g')
+            .attr('id', 'year-list')
+        ;
+        
     }
 
     display(data) {
         this.data = data;
-        console.log('dendrogram data');
-        console.log(data);
 
-        console.log(d3.hierarchy(data).leaves()[1])
         let tree = d3.cluster()
             .size([2 * Math.PI, this.config.width / 2 - 100]) // angle in radians
         ;
-        console.log(tree);
 
         let root = tree(this.bilink(d3.hierarchy(data)
             .sort((a, b) => // this sorts the nodes alphabetically around the edge of the dendrogram.
@@ -30,10 +41,7 @@ class Dendrogram {
             )
         )); 
         
-        console.log('dendrogram tree');
-        console.log(root);
-        
-        const node = this.svg.append("g") // note that the nodes have no mark, just a label
+        const node = this.dendroSvg.append("g") // note that the nodes have no mark, just a label
             .attr('id', 'dendrogram-node-label')
             .selectAll("g")
             .data(root.leaves())
@@ -55,39 +63,88 @@ class Dendrogram {
 
         //let colorin = "#00f";
         //let colorout = "#f00";
-        let colornone = "#ccc";
-
-        let line = d3.lineRadial()
+        
+        let line = d3.lineRadial() // delete one of the two
             .curve(d3.curveBundle.beta(0.85))
             .radius(d => d.y)
             .angle(d => d.x)
         ;
+       
 
-        const link = this.svg.append("g")
+        //outgoing = [myselfnode, linkdestinationnodej, treatyname, yearsigned]
+        let linkData = root.leaves().flatMap(leaf => leaf.outgoing);
+        this.linksByYear = {};
+    
+        for (const o of linkData) {
+            let year = o[3];
+            let d = line(o[0].path(o[1]));
+            if (!(year in this.linksByYear)) {
+                this.linksByYear[year] = [];
+            }
+            if (year != "") {
+                this.linksByYear[year].push([o[2], d]);
+            }
+        }
+
+        let y = 10;
+        let dy = this.config.width / 75;
+        for (let i = 1948; i < 2019; ++i) {
+            let t = this.yearList.append('text')
+                .attr('x', 20)
+                .attr('y', y)
+                .html(i)
+                .on('click', (d, z) => {
+                    this.updateYear(i);
+                })
+            ;
+            y += dy;
+        }
+
+        let colornone = "#ccc";
+        this.dendroSvg.append("g")
+            .attr('id', 'dendro-links')
             .attr("stroke", colornone)
             .attr("fill", "none")
-            .selectAll("path")
-            .data(root.leaves().flatMap(leaf => leaf.outgoing))
-            .join("path")
-            .style("mix-blend-mode", "multiply")
-            .attr("d", ([i, o]) => line(i.path(o)))
-            .each(function(d) { d.path = this; })
         ;
 
+        this.updateYear(2018);
     }
+
+    updateYear(year) {
+        d3.select('#dendro-links')
+            .selectAll('path')
+            .remove()
+        ;
+
+        if (year in this.linksByYear) {
+            d3.select('#dendro-links')
+                .selectAll("path")
+                .data(this.linksByYear[year])
+                .join('path')
+                .style("mix-blend-mode", "multiply")
+                .attr('d', ([name, path]) => path)
+                //.each(function(d) { d.path = this; })
+            ;
+        }
+        
+    }
+
+
 
     bilink(root) {
         const map = new Map(root.leaves().map(d => [d.data.name, d]));
         for (const d of root.leaves()) {
-            d.incoming = [];
+            //d.incoming = [];
+
+            //create outgoing array. outgoing = [myselfnode, linkdestinationnodej, treatyname, yearsigned]
             if (d.data.abcfta) {
-                d.outgoing = d.data.abcfta.map(i => [d, map.get(i.with)]);
+                d.outgoing = d.data.abcfta.map(fta => [d, map.get(fta.with), fta.treaty, fta.year]);
             } else {
                 d.outgoing = [];
             }
         }
         
-        for (const d of root.leaves()) for (const o of d.outgoing) o[1].incoming.push(o);
+        //for (const d of root.leaves()) for (const o of d.outgoing) o[1].incoming.push(o);
         return root;
       }
 }
